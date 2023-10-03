ec91398d7f2ab9db88019a1837a4b7b1aedd7d0a
Pedro Avila <pedroulisesavila@gmail.com>
2023-10-02 19:28:57 -0500

    Implemetacion de Singleton

--- Starter solution/Singleton/Implementation.cs
+++ Starter solution/Singleton/Implementation.cs
@@ -1,4 +1,35 @@
 ﻿namespace Singleton
 {
+
+    public class Logger
+    {
+        // Lazy<T>
+        private static readonly Lazy<Logger> _lazyLogger
+            = new Lazy<Logger>(() => new Logger());
+
+        //private static Logger? _instance;
+
+        public static Logger Instance
+        {
+            get
+            {
+                return _lazyLogger.Value;
+                //if (_instance == null)
+                //    _instance = new Logger();
+                //return _instance;
+            }
+        }
+
+        protected Logger() 
+        {
+        }
+
+        /// <summary>
+        /// SingletonOperation
+        /// </summary>
+        /// <param name="message"></param>
+        public void Log(string message) => Console.WriteLine($"Message to log: {message}");
+        
+    }
   
 }
